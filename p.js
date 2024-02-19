6ae94056c5f7d7cab7ba6dd199e5d98e6203c4d0
ataur rahman <ataurrahmanbro@gmail.com>
2024-02-19 23:24:48 +0600

    main

--- p.js
+++ p.js
@@ -1,24 +1,40 @@
-var found = 45;
-function thisIsFoundFucntio(found){
-    var newOne = 0;
-    if(found <= 10){
-        newOne = 10 * 50;
+// var found = 40;
+// function thisIsFoundFucntio(found){
+//     var newOne = 0;
+//     if(found <= 10){
+//         newOne = 10 * 50;
+//     }
+//     else if(found <= 20){
+//        var firstpart = 10 * 50;
+//        var reminging = found -10;
+//        var secondPart = reminging * 100;
+//        secondPart = firstpart + secondPart;
+//     }
+//     else{
+//         var firstPart = 10 * 20;
+//         var secondPart = 20 * 100;
+//         var reminging = found - 20;
+//         var thirdPart = reminging * 200;
+//         var thirdPart = firstPart + secondPart + thirdPart; 
+//     }
+//     return newOne;
+// }
+
+// var round = thisIsFoundFucntio(10);
+// console.log(round);
+
+var noeasy = ["We know who to change our life"]
+
+function reString(r){
+    var stringr = "";
+    for(var i = 0; i<r.length; i++){
+        var newCharecter = r[i];
+        stringr = stringr + newCharecter;
     }
-    else if(found <= 20){
-       var firstpart = 10 * 50;
-       var reminging = found -10;
-       var secondPart = reminging * 100;
-       secondPart = firstpart + secondPart;
-    }
-    else{
-        var firstPart = 10 * 20;
-        var secondPart = 20 * 100;
-        var reminging = found - 20;
-        var thirdPart = reminging * 200;
-        var thirdPart = firstPart + secondPart + thirdPart; 
-    }
-    return newOne;
+    return stringr;
 }
 
-var round = thisIsFoundFucntio(10);
-console.log(round);
+var nothingToDo = reString(noeasy)
+console.log(nothingToDo);
+
+
